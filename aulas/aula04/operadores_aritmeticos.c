0bd2a1907c7616eced1d0e3d10018e29457e7c38
unknown <2612082029@ACADEMICO.IESB.BR>
2026-04-08 10:51:57 -0300

    Codigos da aula04

---
+++ aulas/aula04/operadores_aritmeticos.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    //1.entrada
+    int operador 1;
+    int operador 2;
+    printf("Entre com um numero inteiro:");
+    scanf("%i",&operador1);
+    while(getchar() !='\n');
+
+    printf("Entre com outro numero inteiro:");
+    scanf("%i",&operador2);
+    while(getchar() !='\n');
+
+    //2.processamento
+    int soma = operador1 + operador2;
+    int subtracao = operador1 - operador2;
+    int multiplicacao = operador1 * operador2;
+    int divisao = operador1 / operador2;
+    int divisao = operador1 % operador2;// modulo, nao é o percentual MOD
+
+    //3.saída
+    printf("%i + %i = %i\n"operador1, operador2, soma);
+    printf("%i - %i = %i\n"operador1, operador2, subtracao);
+    printf("%i * %i = %i\n"operador1, operador2, multiplicacao);
+    printf("%i / %i = %i\n"operador1, operador2, divisao);
+
+    //funcoes matematicas
+    double potencia = pow (2,3);
+    double raiz_quadrada = sqrt (2);
+    double logaritimo = log (2);
+    double seno = sin (2,3); 
+
+return 0;
+
+}
