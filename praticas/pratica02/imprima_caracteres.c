3b0d50e30402bd232f022df13d1716bda5e34e29
unknown <2612082029@ACADEMICO.IESB.BR>
2026-04-08 10:19:25 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+
+    char tecla_pressionada ='A';
+    tecla_pressionada = 'A';
+    printf(" %c \n Isso aqui eh uma string.", tecla_pressionada);
+   
+    return 0;
+}
