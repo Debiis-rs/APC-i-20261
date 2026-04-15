4c5f644bb52959ddb6b64016df57a8a908c6cc38
unknown <2612082029@ACADEMICO.IESB.BR>
2026-04-15 10:53:33 -0300

    Corrigindo conflitop da pratica 01

---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int idade;
+    char sexo;
+    float altura;
+
+
+    printf("Digite sua idade:");
+    scanf("%i", &idade);
+
+    printf("Digite seu sexo(digite 'M' para masculino ou 'F' para feminino): ");
+    scanf(" %c", &sexo);
+
+    printf("Digite sua altura:");
+    scanf("%f", &altura);
+
+    printf("sua idade é: %i\n, seu sexo é: %c\n, sua altura é: %.2f\n", idade, sexo, altura);
+return 0;
+}
