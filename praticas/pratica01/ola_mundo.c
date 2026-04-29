3dcdb8ec185f3893de9a6fed57cc33e22d2ad406
unknown <2612082029@academico.iesb.br>
2026-04-29 09:03:16 -0300

    feat: conclui pratica01. Fecha #ID

--- praticas/pratica01/ola_mundo.c
+++ praticas/pratica01/ola_mundo.c
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-<<<<<<<< HEAD:praticas/pratica01/ola_mundo.c
-#include<stdio.h>
-int main(){  
-    printf("Ola mundo!");
-    return 0;
-}
-========
-#include<stdio.h>
-int main(){  
-    printf("Ola pessoal!");
-    return 0;
-}
-
->>>>>>>> bf4ee25a612f0d3d9472027ca02a2ffdde41d6cc:aulas/aula01/ola_mundo.c
-=======
 #include<stdio.h>
 
-int main(){
+int main(){ 
 
     printf("Ola mundo!\n");
     return 0;
 }
->>>>>>> bf4ee25a612f0d3d9472027ca02a2ffdde41d6cc
+
+
