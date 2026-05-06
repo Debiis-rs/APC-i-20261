61bd753364c3da5d9bac381c27ad7f303a76c977
unknown <2612082029@ACADEMICO.IESB.BR>
2026-05-06 10:38:35 -0300

    codigos da aula 05

---
+++ aulas/aula05/comando_switch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int nota;
+
+    printf("Entrer com uma nota de 1 a 5 :");
+    scanf("%i", &nota);
+
+    if (nota == 1)
+    {
+        //printf("Ganhou uma estrela * \n");
+    }
+    else if (nota == 2)
+    {
+       // printf("Ganhou duas estrela ** \n");
+    }
+    else if (nota == 3)
+    {
+        //printf("Ganhou tres estrela *** \n");
+    }
+    else if (nota == 4)
+    {
+        //printf("Ganhou quatro estrela **** \n");
+    }
+    else if (nota == 5)
+    {
+        //printf("Ganhou cinco estrela ***** \n");
+    }
+     
+    switch (nota) {
+        case 1:   printf("Ganhou uma estrela * \n"); break;
+        case 2:   printf("Ganhou duas estrela ** \n"); break;
+        case 3:   printf("Ganhou tres estrela *** \n"); break;
+        case 4:   printf("Ganhou quatro estrela **** \n"); break;
+        case 5:   printf("Ganhou cinco estrela ***** \n"); break;
+       default:   printf(" Nota invalida! Tente de novo\n");
+    }
+
+    return 0;
+}
