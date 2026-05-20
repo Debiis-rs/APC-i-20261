bf5e23ce5e1f9769beaff8feb5275018d2fdb26b
unknown <2612082029@academico.iesb.br>
2026-05-20 08:29:54 -0300

    feat: conclui pratica03

--- praticas/pratica03/declara_variaveis.c
+++ praticas/pratica03/declara_variaveis.c
@@ -6,16 +6,17 @@ int main(){
     char sexo;
     float altura;
 
-
-    printf("Digite sua idade:");
+    printf("Digite sua idade: ");
     scanf("%i", &idade);
+    while (getchar()!='\n');
 
-    printf("Digite seu sexo(digite 'M' para masculino ou 'F' para feminino): ");
-    scanf(" %c", &sexo);
+    printf("Digite seu sexo (digite 'M' para masculino ou 'F' para feminino): ");
+    scanf(" %c", &sexo); 
 
-    printf("Digite sua altura:");
+    printf("Digite sua altura: ");
     scanf("%f", &altura);
 
-    printf("sua idade é: %i\n, seu sexo é: %c\n, sua altura é: %.2f\n", idade, sexo, altura);
-return 0;
+    printf("Sua idade é: %i\nSeu sexo é: %c\nSua altura é: %.2f\n", idade, sexo, altura);
+
+    return 0;
 }
--- praticas/pratica03/tamanho_tipos.c
+++ praticas/pratica03/tamanho_tipos.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
-
 int main(){
 
+    prinf("Tamanho char : %lu byte\n",sizeof (char));
+    prinf("Tamanho short int : %lu byte\n",sizeof (short unsigned int));
+    prinf("Tamanho int : %lu byte\n",sizeof (unsigned int));
+    prinf("Tamanho  long int: %lu byte\n",sizeof (long unsigned int));
+    prinf("Tamanho long long int: %lu byte\n",sizeof (long long unsigned int));
+    prinf("Tamanho float :%lu byte\n",sizeof (float));
+    prinf("Tamanho double :%lu byte\n",sizeof (double));
+    prinf("Tamanho long double :%lu byte\n",sizeof (long double));
+   
 return 0;
 }
